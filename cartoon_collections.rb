1892c1bd8f55a5048192ef10edc63cb3a9859856
Raz Al-Jaf <razaljaf@gmail.com>
2020-12-05 23:08:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,17 +11,6 @@ def summon_captain_planet(planeteer_calls)
     new_array << "#{planeteer_calls.capitalize}!"
   end
 end
-def long_planeteer_calls(planeteer_calls)
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
-  index=0
-  array.count do |value|
-    if value> 4.characters== true
-  else puts "false"
-end
-end
-end
-end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   valid_calls.find planeteer_calls do |valid_calls|
